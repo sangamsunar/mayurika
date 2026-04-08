8b2b429d2a0c05f24a0cd264e213c587a6060ff6
Sangam Sunar <you@example.com>
2026-04-08 09:23:14 +0545

    changes

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,15 +5,25 @@ const userSchema = Schema({
     email: { type: String, unique: true },
     password: String,
     isVerified: { type: Boolean, default: false },
-    role: {
-        type: String,
-        enum: ['user', 'admin', 'guest'],
-        default: 'user'
-    },
+    role: { type: String, enum: ['user', 'admin', 'guest'], default: 'user' },
     resetOtp: String,
     otpExpiry: Date,
     verifyOtp: String,
-    verifyOtpExpiry: Date
+    verifyOtpExpiry: Date,
+    cart: [
+        {
+            product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+            selectedMetal: String,
+            selectedPurity: String,
+            selectedWeight: Number,
+            quantity: { type: Number, default: 1 }
+        }
+    ],
+    wishlist: [
+        {
+            product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' }
+        }
+    ]
 })
 
 const UserModel = mongoose.model('User', userSchema)
